1a34ecbce2dba880a55dcf87340e9a266068e507
blastnote <nickbrinthaupt@gmail.com>
2021-03-09 15:48:40 -0500

    Created dynamic item page empty

---
+++ pages/Item/_id/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-chakra display="flex" pos="relative" w="100vw" h="100vh">
+        <c-box alignSelf="center" justifySelf="center" mx="auto" w="100px" h="100px" bg="red.900">
+
+        </c-box>
+    </div>
+</template>
+
+<script>
+import * as dummyDataAPI from '~/assets/js/dummyDataAPI.js'
+import { CBox } from '@chakra-ui/vue'
+
+export default {
+    components: {
+        CBox
+    },
+    data() {
+        return {
+            item: {}
+        }
+    },
+    mounted() {
+        this.item = dummyDataAPI.getItem(this.$route.params.id)
+    }
+}
+</script>
