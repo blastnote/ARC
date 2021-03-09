76c9e0a3775deb0f8a841cb9974423319ff79f49
blastnote <nickbrinthaupt@gmail.com>
2021-03-09 15:47:19 -0500

    Added item call

--- assets/js/dummyDataAPI.js
+++ assets/js/dummyDataAPI.js
@@ -1,5 +1,12 @@
 import data from '~/assets/js/dummyData';
 
-export function getCards(cardsNum) {
-    return data['cards'].slice(0, cardsNum)
+export function getItems(itemsNum) {
+    return data['items'].slice(0, itemsNum)
+}
+
+export function getItem(itemId) {
+    for (let i = 0; i < data['items'].length; i++) {
+        if (data['items'][i].id == itemId) { return data['items'][i]; }
+    }
+    return -1;
 }
