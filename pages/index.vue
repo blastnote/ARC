e71f05753c17efb6f1268601f0b144fb21a994c1
blastnote <nickbrinthaupt@gmail.com>
2021-03-16 16:24:32 -0400

    Initial start to login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,22 @@
 <template>
-    <c-box display="flex">
-        <ItemCard v-chakra m="2" title="Test item 1" />
-        <ItemCard v-chakra m="2" title="Test item 2" image="https://images.unsplash.com/photo-1579197708794-8a6e76025da9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=659&q=80" />
-        <ItemCard v-chakra m="2" title="Test item 3" image="https://images.unsplash.com/photo-1579207114602-bfbaed47835d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
-        <ItemCard v-chakra m="2" title="Test item 4" />
-        <ItemCard v-chakra m="2" title="Test item 5" image="https://images.unsplash.com/photo-1613328007570-379c5b22317d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
+    <c-box style="background-image: linear-gradient(to top, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.3) 20%, rgba(0,0,0,0.3) 80%, rgba(0,0,0,0.65) 100%), url('https://images.unsplash.com/photo-1496096265110-f83ad7f96608?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80');" 
+        w="100vw" h="100vh" bgPos="center" bgRepeat="no-repeat" bgSize="cover" display="flex">
+        <c-box  alignSelf="center" justifyItems="center" justifySelf="center" mx="auto" w="400px" h="600px" bg="white" rounded="5px">
+            <c-text></c-text>
+
+        </c-box>
     </c-box>
 </template>
 
 <script>
-import ItemCard from '@/components/ItemCard'
-import { CBox } from '@chakra-ui/vue'
+import { CBox, CText, CInput } from '@chakra-ui/vue'
 
 export default {
     name: "App",
-    components: [
-        ItemCard,
-        CBox
-    ]
+    components: {
+        CBox,
+        CText,
+        CInput
+    }
 }
 </script>
